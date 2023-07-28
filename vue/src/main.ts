3ef278ad0d757ff0f7c654d27cdf537102b1aa6a
Jonas Bjørge Andersen <dev@jonasandersen.no>
2023-07-28 11:44:12 +0200

    Initial commit

---
+++ vue/src/main.ts
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { init } from "@neutralinojs/lib"
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
+
+init()
